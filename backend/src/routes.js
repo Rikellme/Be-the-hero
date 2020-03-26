bad3bbc1fc2f63f08bbf58f5ae55d00156ce586b
Rikellme Keven <56124549+Rikellme@users.noreply.github.com>
2020-03-26 19:45:03 -0300

    Update routes.js

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -7,7 +7,7 @@ const SessionController = require('./controllers/SessionController');
 
 const routes = express.Router();
 
-routes.post('/session', SessionController.create);
+routes.post('/sessions', SessionController.create);
 
 routes.get('/ongs', OngController.index);
 routes.post('/ongs', OngController.create);
